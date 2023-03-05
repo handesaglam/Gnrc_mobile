8375d88b97ff84d00d0389c62e0498125862e9f4
Hande SAĞLAM <63313545+handesaglam@users.noreply.github.com>
2023-03-05 09:16:03 +0300

    Initial Commit

---
+++ Gnrc_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gnrc_mobile
+//
+//  Created by Hande Sağlam on 5.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
